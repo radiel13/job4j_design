e39ed4af3a47ee2a8bff832bf91a12cfd6cb2665
radiel <radiel13@gmail.com>
2021-09-30 22:08:39 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generic/UserStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserStore<T extends Base> implements Store<User> {
+
+    private final Store<User> store = new MemStore<>();
+
+
+    @Override
+    public void add(User model) {
+        store.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, User model) {
+        return store.replace(id, model);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return store.delete(id);
+    }
+
+    @Override
+    public User findById(String id) {
+        return store.findById(id);
+    }
+}
