a16075a4df7e6b42373dbb3e73a4d4e125c5af65
radiel <radiel13@gmail.com>
2022-02-09 21:03:50 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("There is no such a key");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+         for (int i = 0; i < args.length; i++) {
+            String key = args[i].substring(1, args[i].indexOf("="));
+            String value = args[i].substring(args[i].indexOf("=") + 1);
+            if (value.isEmpty()) {
+                throw new IllegalArgumentException("There is no value");
+            }
+            values.put(key, value);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+        System.out.println(jvm.get("encoding"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+        System.out.println(zip.get("encoding"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class ArgsNameTest {
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenMultipleEqualsSymbol() {
+        ArgsName jvm = ArgsName.of(new String[] {"-request=?msg=Exit="});
+        assertThat(jvm.get("request"), is("?msg=Exit="));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        jvm.get("Xmx");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {"-=a"});
+        jvm.get("key");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongSomeArgument() {
+        ArgsName jvm = ArgsName.of(new String[] {"-enconding=UTF-8", "-Xmx="});
+    }
+}
