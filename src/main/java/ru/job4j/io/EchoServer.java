c53873cdcc2abf6896d6dc5f107dc1b83264b18d
radiel <radiel13@gmail.com>
2022-05-03 18:07:25 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,10 +12,17 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("/?msg=Bye")){
+                        if (str.equals("GET /?msg=Hello HTTP/1.1")) {
+                            out.write("\r\nHello.".getBytes());
+                            break;
+                        } else if (str.equals("GET /?msg=Exit HTTP/1.1")) {
                             server.close();
+                        } else {
+                            out.write("\r\nWhat".getBytes());
+                            break;
                         }
                     }
                     out.flush();
