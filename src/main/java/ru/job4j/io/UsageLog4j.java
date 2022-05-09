bf46c042489d07688a2b49b1b67598d3ca4051e6
radiel <radiel13@gmail.com>
2022-05-09 15:58:57 +0300

    1. Log4j. Логирование системы. [#95335]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,11 +1,11 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
@@ -13,5 +13,24 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        String name = "Paul";
+        int age = 29;
+        byte legs = 2;
+        short height = 176;
+        long salary = 10000L;
+        float weight = 68.2f;
+        double tripple = 123.8D;
+        boolean sex = true;
+        char firstLetter = 'P';
+        LOG.debug("User info name : {}, " +
+                "age : {}, " +
+                "legs : {}, " +
+                "height : {}, " +
+                "salary : {}, weight : {},  " +
+                "tripple : {}, " +
+                "sex : {}, " +
+                "firstLetter : {}" ,
+                name, age, legs,height, salary, weight, tripple, sex, firstLetter);
     }
 }
